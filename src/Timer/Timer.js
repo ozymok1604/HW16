79e0acbe8c011dcd5b54462a0e41e2e34b714ac3
ozymok1604 <ozimok1604ua@gmail.com>
2022-05-16 16:08:09 +0300

    first commit

---
+++ src/Timer/Timer.js
@@ -0,0 +1,52 @@
+import React, { Component, useEffect, useReducer } from "react";
+import "./Timer.css";
+import { useState } from "react";
+const addZero = (time) => {
+  return time.toString().padStart(2, "0");
+};
+function Timer() {
+  const [timeLeft, setTimeLeft] = useState(2 * 60);
+  const [isCounting, setIsCounting] = useState(false);
+  const [progress, setProgress] = useState(100);
+  const minutes = addZero(Math.floor(timeLeft / 60));
+  const seconds = addZero(timeLeft - minutes * 60);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      isCounting &&
+        setTimeLeft((timeLeft) => (timeLeft >= 1 ? timeLeft - 1 : 0));
+      isCounting &&
+        setProgress((progress) =>
+          progress >= 1 ? progress - 100 / timeLeft : 0
+        );
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isCounting]);
+
+  const handleStart = () => {
+    setIsCounting(true);
+  };
+  const handleStop = () => {
+    setIsCounting(false);
+  };
+
+  return (
+    <div class="timer">
+      <div class="time">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+      <div class="buttons">
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+      </div>
+      <div className="progress-loader" style={{ width: `${progress}%` }}>
+        ' '
+      </div>
+    </div>
+  );
+}
+export { Timer };
